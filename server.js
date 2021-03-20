8c84c39cd4375ff2bfaf3ee5ef50a7270a8e8ceb
Arpan Neupane <arpanneupane19@gmail.com>
2021-03-20 14:33:41 -0500

    added disconnect feature and user reconnection

--- server.js
+++ server.js
@@ -11,20 +11,24 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Socket setup
 const io = socket(server);
 
-let rooms = [];
+
+// Botname for sending bot messages
 let botName = 'SecuroChat Bot'
+
+// map users to their respective room
 let users = new Map();
 
+// Map id to username
+let id = new Map();
+
+// map rooms to the users inside
+let rooms = new Map();
+
+
+
+
 app.get('/:code', function (req, res) {
-    let codeFound = false;
-
-    for (let i = 0; i < rooms.length; i++) {
-        if (rooms[i].roomCode === req.params.code) {
-            codeFound = true;
-        }
-    }
-
-    if (codeFound) {
+    if (rooms.has(req.params.code)) {
         res.sendFile(path.join(__dirname, 'public', 'chat.html'));
     } else {
         res.redirect('/');
@@ -35,45 +39,76 @@ io.on('connection', (socket) => {
     console.log('made connection', socket.id)
 
 
-    // Welcome user
-    socket.emit('message', `${botName}: Hello, welcome to SecuroChat!`)
-
     socket.on('createRoom', (data) => {
-        rooms.push(data)
-        console.log(rooms);
-        socket.join(data.roomCode);
+        rooms.set(data.roomCode, [data.user])
         users.set(data.user, data.roomCode);
-        console.log(users);
+        socket.join(data.roomCode);
     })
 
     socket.on('connectUser', (username) => {
         if (users.has(username)) {
             socket.join(users.get(username))
-            socket.to(users.get(username)).emit("message", `${username} has joined the chat.`)
+            console.log(id.values())
+            let userFound = false;
+            for (let name of id.values()) {
+                if (name === username) {
+                    userFound = true;
+                }
+            }
+            id.set(socket.id, username);
+
+            if (userFound) {
+                socket.emit('botMessage', `${botName}: You've rejoined.`)
+            }
+            else {
+                // Welcome user
+                socket.emit('botMessage', `${botName}: Hello, welcome to SecuroChat!`)
+                socket.to(users.get(username)).emit("botMessage", `${username} has joined the chat.`)
+            }
         };
     })
-
     socket.on('joinRoom', (data) => {
-        let codeFound = false;
-        for (let i = 0; i < rooms.length; i++) {
-            if (rooms[i].roomCode === data.roomCode && data.user !== '') {
-                codeFound = true
+        if (rooms.has(data.roomCode)) {
+            // Check for the array of users mapped to the room code
+            let arrayOfUsers = rooms.get(data.roomCode);
+
+            if (!arrayOfUsers.includes(data.user)) {
+                arrayOfUsers.push(data.user);
+                rooms.set(data.roomCode, arrayOfUsers);
+                console.log(rooms);
             }
-        }
-        if (codeFound) {
-            rooms.push(data)
-            console.log(rooms)
+
+            if (!users.has(data.user)) {
+                users.set(data.user, data.roomCode);
+            }
+
             socket.join(data.roomCode)
-            users.set(data.user, data.roomCode);
-        }
-        if (!codeFound) {
-            console.log('Code does not exist.')
         }
     })
 
-    // socket.on('message', (message) => {
-    //     console.log(message)
-    //     socket.emit('incomingMessage', (message));
-    // })
+    socket.on('disconnect', (reason) => {
+        if (id.has(socket.id)) {
+            let username = id.get(socket.id)
+            let userCount = 0;
+            for (let name of id.values()) {
+                if (name === username) {
+                    userCount++;
+                }
+            }
+
+            if (userCount === 1) {
+                // Send a message when a user disconnects fully
+                socket.to(users.get(username)).emit('botMessage', `${username} has left the chat.`)
+                users.delete(username);
+            }
+
+            id.delete(socket.id);
+
+        }
+
+
+    })
+
+
 
 })
